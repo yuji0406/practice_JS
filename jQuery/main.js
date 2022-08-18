2863861f9c74c56212105eb102fef0c10b49818d
yuji0406 <uver_35@yahoo.co.jp>
2022-08-18 23:05:17 +0900

    finish jQuery lesson

--- jQuery/main.js
+++ jQuery/main.js
@@ -1,13 +1,9 @@
-var flg = false;
 $(function () {
-    $('#btn1').click(function() {
-        var id = $('#text1').val();
-        $.getJSON('data.php', {'id':id}, function(response, status, xhr) {
-            var ol = $('<ul></ul>');
-            ol.append('<li>名前:' + response.name + '</li>');
-            ol.append('<li>メールアドレス:' + response.mail + '</li>');
-            ol.append('<li>電話番号:' + response.tel + '</li>');
-            $('#msg').empty().append(ol);
-        });
+    var flg = false;
+    $('#image').click(function () {
+        $('#frame').slideUp(500);
+    });
+    $('#btn1').click(function () {
+        $('#frame').slideDown(500);
     });
 });
