91ee459f8036f88470f0d3603a6b9898a7b29c9f
yuji0406 <uver_35@yahoo.co.jp>
2022-08-24 22:25:44 +0900

    継承

--- TypeScript/hello.js
+++ TypeScript/hello.js
@@ -1,28 +1,59 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Person = /** @class */ (function () {
     function Person(n, y) {
         this.name = n;
         this.age = y;
     }
-    Object.defineProperty(Person.prototype, "age", {
-        get: function () {
-            return this._age;
-        },
-        set: function (y) {
-            this._age = y < 0 ? 0 : y;
-        },
-        enumerable: false,
-        configurable: true
-    });
     Person.prototype.print = function () {
         var msg = '<p>My name is ' + this.name + '. I am ' + this.age + 'years old. </p>';
         return msg;
     };
     return Person;
 }());
+var Student = /** @class */ (function (_super) {
+    __extends(Student, _super);
+    function Student(n, y, g) {
+        var _this = _super.call(this, n, y) || this;
+        _this.grade = g;
+        return _this;
+    }
+    Student.prototype.printAll = function () {
+        var msg = '<p>My name is '
+            + this.name
+            + '. I am '
+            + this.age
+            + ' years old. '
+            + 'My grade is'
+            + this.grade
+            + '. </p>';
+        return msg;
+    };
+    return Student;
+}(Person));
 function doClick() {
     var val = document.querySelector('#text1').value;
-    var arr = val.split(',');
-    var obj = new Person(arr[0], parseInt(arr[1]));
     var msg = document.querySelector('#msg');
-    msg.innerHTML = obj.print();
+    var arr = val.split(',');
+    if (arr.length == 2) {
+        var obj = new Person(arr[0], parseInt(arr[1]));
+        msg.innerHTML = obj.print();
+    }
+    else if (arr.length == 3) {
+        var obj = new Student(arr[0], parseInt(arr[1]), parseInt(arr[2]));
+        msg.innerHTML = obj.printAll();
+    }
 }
--- TypeScript/hello.ts
+++ TypeScript/hello.ts
@@ -1,13 +1,6 @@
 class Person {
     public name: string;
-    private _age: number;
-
-    get age(): number {
-        return this._age;
-    }
-    set age(y: number) {
-        this._age = y < 0 ? 0 : y;
-    }
+    public age: number;
 
     constructor(n: string, y: number) {
         this.name = n;
@@ -20,12 +13,41 @@ class Person {
     }
 }
 
+class Student extends Person {
+    public grade: number;
+
+    constructor(n: string, y: number, g: number) {
+        super(n, y);
+        this.grade = g;
+    }
+
+    printAll():string {
+        let msg: string = 
+            '<p>My name is ' 
+            + this.name 
+            + '. I am ' 
+            + this.age 
+            + ' years old. '
+            + 'My grade is'
+            + this.grade 
+            + '. </p>';
+        return msg;
+    }
+}
+
+
 function doClick(): void {
     let val: string = document.querySelector('#text1').value;
+    let msg: Element = document.querySelector('#msg');
+
     let arr: string[] = val.split(',');
 
-    let obj: Person = new Person(arr[0], parseInt(arr[1]));
+    if (arr.length == 2) {
+        let obj: Person = new Person(arr[0], parseInt(arr[1]));
+        msg.innerHTML = obj.print();
+    } else if (arr.length == 3) {
+        let obj: Student = new Student(arr[0], parseInt(arr[1]), parseInt(arr[2]))
+        msg.innerHTML = obj.printAll();
+    }
 
-    let msg: Element = document.querySelector('#msg');
-    msg.innerHTML = obj.print();
 }
