5d93acccbf5ac1e9c2a4880b49d840b6e407c806
yuji0406 <uver_35@yahoo.co.jp>
2022-08-26 22:07:22 +0900

    conputedを使って変数を加工

--- vue.js/main.js
+++ vue.js/main.js
@@ -3,18 +3,15 @@ function initial() {
     new Vue({
         el: '#msg',
         data: {
-            msgArray: [],
-            message: '',
-            text1: ''
+            number: '0'
         },
-        created: function () {
-            this.msgArray.push('sample message.');
-            this.message  = this.msgArray;
-        },
-        methods: {
-            doAction: function () {
-                this.msgArray.push(this.text1);
-                this.message = this.msgArray;
+        computed: {
+            result: function () {
+                var total = 0;
+                for(var i = 0; i <= this.number * 1; i++) {
+                    total += i;
+                }
+                return total;
             }
         }
     })
