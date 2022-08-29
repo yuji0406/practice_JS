8b7dcdad338bde569d2bcbf93b8c6bc611574361
yuji0406 <uver_35@yahoo.co.jp>
2022-08-29 20:41:13 +0900

    v-forで繰り返し処理

--- vue.js/main.js
+++ vue.js/main.js
@@ -3,10 +3,7 @@ function initial() {
     new Vue({
         el: '#msg',
         data: {
-            flg: false
-        },
-        created: function () {
-            this.flg = true;
-        },
+            data: []
+        }
     });
 }
